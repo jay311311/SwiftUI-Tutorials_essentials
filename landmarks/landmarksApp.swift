3db0633bfde13ee623519163ec0e5167998c258e
jooeun kim <jay311311@gmail.com>
2022-03-31 12:25:33 +0900

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Jooeun Kim on 2022/03/31.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
