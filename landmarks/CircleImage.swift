bea7050cf6850bfbf69596ab3fb781ab759fb50d
jooeun kim <jay311311@gmail.com>
2022-03-31 14:34:31 +0900

    tutorial_01_Creating and Combining Views

---
+++ landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  landmarks
+//
+//  Created by Jooeun Kim on 2022/03/31.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.blue, lineWidth: 4.0)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
