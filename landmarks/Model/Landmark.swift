cab78d1739976e9af446ac705c97faa8d73e00af
jooeun kim <jay311311@gmail.com>
2022-03-31 19:47:25 +0900

    Tutorial_02_buildingList and Navigation

---
+++ landmarks/Model/Landmark.swift
@@ -0,0 +1,39 @@
+//
+//  Landmark.swift
+//  landmarks
+//
+//  Created by Jooeun Kim on 2022/03/31.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark:Codable, Hashable, Identifiable{
+    var id : Int
+    var name : String
+    var park :  String
+    var state : String
+    var description : String
+    
+    // - private1 : 사용자가 보여질 이미지 자체에만 신경쓰기때문에 private사용함
+    private var imageName : String
+    var image : Image{
+        Image(imageName)
+    }
+    
+    // - private2 : 계산된 property를 만드는데에만 사용될것이기 때문에 private 사용함
+    private var coordinates : Coordinates
+    var locationCoordinate : CLLocationCoordinate2D{
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    struct Coordinates : Codable, Hashable{
+        var latitude : Double
+        var longitude : Double
+    }
+
+}
--- landmarks/Views/LandmarkDetail.swift
+++ landmarks/Views/LandmarkDetail.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LandmarkDetail.swift
 //  landmarks
 //
 //  Created by Jooeun Kim on 2022/03/31.
@@ -7,44 +7,48 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
     var body: some View {
-        VStack{
-            MapView()
+        ScrollView{
+            MapView(coordinate: landmark.locationCoordinate)
                 .frame( height: 300)
                 .ignoresSafeArea( edges: .top)
-            CircleImage()
+            
+            CircleImage(image: landmark.image)
                 .offset( y: -130)
                 .padding(.bottom, -130)
+            
             VStack(alignment: .leading) {
-                Text("Hello, Turtle Rock")
+                Text(landmark.name)
                     .font(.title)
                 
                 HStack {
-                    Text("Joshua Tree National Park")
+                    Text(landmark.park)
                     Spacer()
-                    Text("California")
+                    Text(landmark.state)
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
                 
                 Divider()
                 
-                Text("About Turtle Rock")
+                Text("About \(landmark.name)")
                     .font(.title2)
                 
-                Text("Descriptive text goes here.")
+                Text(landmark.description)
             }
             .padding()
             
             Spacer()
         }
-    
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail(landmark: landmarks[0])
     }
 }
